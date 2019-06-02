c86e5a187bed15ac9df9b7cc4a5ccd109ba9bb0b
Young Bin Kim <lyakyb@gmail.com>
2019-06-02 15:57:38 -0400

    install typography plugin

---
+++ src/utils/typography.js
@@ -0,0 +1,19 @@
+import Typography from 'typography'
+
+const fontFamilies = [
+  "Raleway",
+  "Roboto",
+  "Ubuntu",
+  "Helvetica",
+  "Arial",
+  "sans-serif"
+]; 
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: fontFamilies,
+  bodyFontFamily: fontFamilies
+})
+
+export default Typography;
