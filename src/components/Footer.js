2ced386201f2afee93f76666f387406dcc7d84a9
Young Bin Kim <lyakyb@gmail.com>
2019-06-02 17:14:39 -0400

    add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <a href="https://www.github.com/lyakyb" target="_blank">
+          Github
+        </a>
+        <a href="" target="_blank">
+          LinkedIn
+        </a>
+      </footer>
+    )
+  }
+}
