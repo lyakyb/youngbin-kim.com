4d005fef8c154d654c4cd4fe48c4a707b1e85bca
Young Bin Kim <lyakyb@gmail.com>
2019-06-06 07:54:39 -0400

    WIP

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,14 @@ import React from "react"
 import { ImageForFileName } from "../helpers/imageProvider"
 
 export default class Card extends React.Component {
+
   render() {
-    const { img, position, startDate, endDate, techStack } = this.props.data
+    const { img, position, startDate, endDate, techStack, description } = this.props.data
+    const techStacks = techStack.map(tech => {
+      const img = ImageForFileName(tech)
+      return <img src={ImageForFileName(tech)} key={tech} />
+    });
+
     return (
       <div className="card">
         <div className="card-container">
@@ -14,14 +20,15 @@ export default class Card extends React.Component {
             <div className="title">{position}</div>
             <div className="duration">{`${startDate} - ${endDate}`}</div>
             <div className="tech-stack">
-              {techStack.map(tech => {
-                const img = ImageForFileName(tech)
-                return <img src={ImageForFileName(tech)} key={tech} />
-              })}
+              {techStacks}
             </div>
           </div>
           <div className="card-back">
-              LOL
+            <div className="description">
+              {description}
+            </div>
+            <div className="tech-stack">
+            </div>
           </div>
         </div>
       </div>
