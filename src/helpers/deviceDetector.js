b48cf05a3fdff5c81aacce92b010748affaaba39
Young Bin Kim <lyakyb@gmail.com>
2019-09-05 10:20:45 -0400

    fix build

--- src/helpers/deviceDetector.js
+++ src/helpers/deviceDetector.js
@@ -1,2 +1,2 @@
-export const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
+export const isChrome = typeof window !== 'undefined' && !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
 
