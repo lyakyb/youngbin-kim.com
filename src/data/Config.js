6d9d55f86df1b24da2e1572e34575aef0b031a3c
Young Bin Kim <lyakyb@gmail.com>
2019-09-05 10:45:09 -0400

    remove bio

--- src/data/Config.js
+++ src/data/Config.js
@@ -2,7 +2,6 @@ const config = {
   siteTitle: 'Bin Kim',
   navLinks: [
     { name: 'Bio', link: '/bio/'},
-    { name: 'Blog', link: '/404'},
     { name: 'Clips', link: '/clips'},
     { name: 'Contact', link: '/contact'},
   ]
